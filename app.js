b27eff872e256fd4e46f2dbaee3462983d9e2ddc
Sylvain Conan <sylvain.mj.conan@gmail.com>
2025-01-13 11:24:20 +0100

    fix cors error

--- app.js
+++ app.js
@@ -1,33 +1,38 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const bodyParser = require('body-parser');
+app.use(
+  cors({
+    origin: "http://localhost:4200",
+  })
+);
+const bodyParser = require("body-parser");
 
-const User = require('./models/user');
-const UserService = require('./services/userService');
-const UserController = require('./controllers/userController');
-const UserRoutes = require('./routes/userRoutes');
-const registerRoutes = require('./routes/registerRoutes')
+const User = require("./models/user");
+const UserService = require("./services/userService");
+const UserController = require("./controllers/userController");
+const UserRoutes = require("./routes/userRoutes");
+const registerRoutes = require("./routes/registerRoutes");
 
-const Status = require('./models/status');
-const StatusService = require('./services/statusService');
-const StatusController = require('./controllers/statusController');
-const StatusRoutes = require('./routes/statusRoutes');
+const Status = require("./models/status");
+const StatusService = require("./services/statusService");
+const StatusController = require("./controllers/statusController");
+const StatusRoutes = require("./routes/statusRoutes");
 
-const Match = require('./models/match');
-const MatchService = require('./services/matchService');
-const MatchController = require('./controllers/matchController');
-const MatchRoutes = require('./routes/matchRoutes');
+const Match = require("./models/match");
+const MatchService = require("./services/matchService");
+const MatchController = require("./controllers/matchController");
+const MatchRoutes = require("./routes/matchRoutes");
 
-const UserRole = require('./models/userRole');
-const UserRoleService = require('./services/userRoleService');
-const UserRoleController = require('./controllers/userRoleController'); 
-const UserRoleRoutes = require('./routes/userRoleRoutes');
+const UserRole = require("./models/userRole");
+const UserRoleService = require("./services/userRoleService");
+const UserRoleController = require("./controllers/userRoleController");
+const UserRoleRoutes = require("./routes/userRoleRoutes");
 
-const loginController = require('./controllers/loginController');
-const loginService = require('./services/loginService');
-const loginRoutes = require('./routes/loginRoute');
-const logoutRoutes = require('./routes/logoutRoute')
-
+const loginController = require("./controllers/loginController");
+const loginService = require("./services/loginService");
+const loginRoutes = require("./routes/loginRoute");
+const logoutRoutes = require("./routes/logoutRoute");
 
 // Initialiser les services et contrôleurs
 const userService = new UserService(User);
@@ -44,29 +49,23 @@ const matchController = new MatchController(matchService);
 const matchRoutes = MatchRoutes(matchController);
 
 const userRoleService = new UserRoleService(UserRole);
-const userRoleController = new UserRoleController(userRoleService); 
+const userRoleController = new UserRoleController(userRoleService);
 const userRoleRoutes = UserRoleRoutes(userRoleController);
 
 const loginServiceInstance = new loginService();
 const loginControllerInstance = new loginController(loginServiceInstance);
-const loginRoutesInstance = loginRoutes(loginControllerInstance);  
-const logoutRoutesInstance = logoutRoutes(loginControllerInstance);  
+const loginRoutesInstance = loginRoutes(loginControllerInstance);
+const logoutRoutesInstance = logoutRoutes(loginControllerInstance);
 
 app.use(bodyParser.json());
 
-
 // Utiliser les routes
-app.use('/api/users', userRoutes);
-app.use('/api/userRoute', userRoleRoutes);
-app.use('/api/statuses', statusRoutes);
-app.use('/api/matches', matchRoutes);
-app.use('/api/login', loginRoutesInstance); 
-app.use('/api/logout', logoutRoutesInstance); 
-app.use('/api/register', registerRoutesLog); 
-
+app.use("/api/users", userRoutes);
+app.use("/api/userRoute", userRoleRoutes);
+app.use("/api/statuses", statusRoutes);
+app.use("/api/matches", matchRoutes);
+app.use("/api/login", loginRoutesInstance);
+app.use("/api/logout", logoutRoutesInstance);
+app.use("/api/register", registerRoutesLog);
 
 module.exports = app;
-
-
-
-
