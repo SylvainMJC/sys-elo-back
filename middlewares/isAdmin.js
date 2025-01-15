e4731a1cc4b2e0dcfc1e3d2f62d817799d260baa
alex <81435330+Al-vallon@users.noreply.github.com>
2025-01-15 18:17:12 +0100

    .

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -1,21 +1,21 @@
-const  UserRole  = require('../models/userRole');
-const Role = require('../models/role');
+// const  UserRole  = require('../models/userRole');
+// const Role = require('../models/role');
 
 const isAdmin = async (req, res, next) => {
-    try {
-        const userRole = await UserRole.findOne({
-            where: { id_user: req.user.id },
-            include: [Role]
-        });
+//     try {
+//         const userRole = await UserRole.findOne({
+//             where: { id_user: req.user.id },
+//             include: [Role]
+//         });
 
-        if (!userRole || userRole.Role.name !== 'admin') {
-            return res.status(403).send({ error: 'You do not have admin rights.' });
-        }
+//         if (!userRole || userRole.Role.name !== 'admin') {
+//             return res.status(403).send({ error: 'You do not have admin rights.' });
+//         }
 
-        next();
-    } catch (error) {
-        res.status(500).send({ error: 'Internal Server Error' });
-    }
+//         next();
+//     } catch (error) {
+//         res.status(500).send({ error: 'Internal Server Error' });
+//     }
 };
 
 module.exports = isAdmin;
