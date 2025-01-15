da336e0540c8354504ff5db8c13c015e72878ba2
alex <81435330+Al-vallon@users.noreply.github.com>
2025-01-15 20:42:34 +0100

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const sequelize = require('../config/db');
-const Match = require('../models/match');
-const User = require('../models/user'); 
-const Status = require('../models/status');
+const Match = require('./match');
+const User = require('./user'); 
+const Status = require('./status');
 
 // Ajoutez les modèles à un objet
 const models = {
