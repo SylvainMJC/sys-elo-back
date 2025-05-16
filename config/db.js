863c33fe4b3fa87de9ec66b78992c40b00b6c7f5
alex <81435330+Al-vallon@users.noreply.github.com>
2025-05-16 14:56:29 +0200

    Revert "comment out dialectOptions for SSL configuration in Sequelize"
    This reverts commit 2033aecb24f206535f3f408461880bbaaaef8f07.

--- config/db.js
+++ config/db.js
@@ -15,13 +15,12 @@ const sequelize = new Sequelize(
     port: process.env.DB_PORT,
     dialect: "postgres",
     logging: false,
-    //prod
-    // dialectOptions: {
-    //   ssl: {
-    //     require: true, // Enforce SSL
-    //     rejectUnauthorized: false, // Allow self-signed certificates
-    //   },
-    // },
+    dialectOptions: {
+      ssl: {
+        require: true, // Enforce SSL
+        rejectUnauthorized: false, // Allow self-signed certificates
+      },
+    },
   }
 );
 
